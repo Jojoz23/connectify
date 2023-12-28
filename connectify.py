b17c3f7931f19b7bce8c729e4e9c075e1cc7af3b
Rayyan Aamir <rayyan.aamir@mail.utoronto.ca>
2023-12-28 10:25:42 -0500

    launches begin.html

--- connectify.py
+++ connectify.py
@@ -2,13 +2,14 @@
 connectify application launcher. Opens in default browser.
 """
 
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
+app.config["STATIC_FOLDER"] = 'static'  # set location of static folder
 
 @app.route('/')
 def index():
-    return render_template('profile.html')
+    return render_template('begin.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
