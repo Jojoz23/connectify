eace16379daaf550a1636acc3988fd69b9ed724d
Rayyan Aamir <rayyan.aamir@mail.utoronto.ca>
2023-12-28 10:54:53 -0500

    profile display route

--- connectify.py
+++ connectify.py
@@ -8,8 +8,22 @@ app = Flask(__name__)
 app.config["STATIC_FOLDER"] = 'static'  # set location of static folder
 
 @app.route('/')
-def index():
+def index() -> str:
+    """
+    Log in/sign up page.
+    """
     return render_template('begin.html')
+
+@app.route('/profile/<username>')
+def user_profile(username: str) -> str:
+    """
+    Profile of specified username.
+    """
+    # fetch user info
+    user_data = None
+
+    return render_template('profile.html', data=user_data)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
